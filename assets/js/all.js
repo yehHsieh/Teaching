7b892720ab37c7fcef29c2b71b8d5056c6cb0955
york <york0704@gmail.com>
2022-08-31 12:23:38 +0800

    Update 2022-08-31T04:23:32.266Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,28 +1,26 @@
-$(function() {
+"use strict";
+
+$(function () {
   console.log('Hello Bootstrap5');
-});
+}); // <!-- Initialize Swiper -->
 
-
-
-// <!-- Initialize Swiper -->
 var swiper = new Swiper(".ClassSwiper", {
   centeredSlides: true,
   // autoplay: {
   //   delay: 5000,
   //   disableOnInteraction: false,
   // },
-    speed: 1500,
-    loop: true,
-    spaceBetween: 8,
-    slidesPerView: "auto",
-    grabCursor: true,
+  speed: 1500,
+  loop: true,
+  spaceBetween: 8,
+  slidesPerView: "auto",
+  grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+    clickable: true
+  }
+}); // 
 
-// 
 var swiper = new Swiper('.abilitySwiper', {
   // Optional parameters
   effect: 'fade',
@@ -30,33 +28,31 @@ var swiper = new Swiper('.abilitySwiper', {
   fadeEffect: {
     crossFade: true
   },
-  
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
-    dynamicBullets: true,
+    dynamicBullets: true
   },
-
   // Navigation arrows
   navigation: {
     nextEl: '.material-icons',
-    prevEl: '.material-icons',
-  },
-});
+    prevEl: '.material-icons'
+  }
+}); //
 
-//
 var swiper = new Swiper(".AllClassSwiper", {
   centeredSlides: true,
   // autoplay: {
   //   delay: 5000,
   //   disableOnInteraction: false,
   // },
-    speed: 1500,
-    spaceBetween: 8,
-    slidesPerView: "auto",
-    grabCursor: true,
+  speed: 1500,
+  spaceBetween: 8,
+  slidesPerView: "auto",
+  grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
-    clickable: true,
-  },
+    clickable: true
+  }
 });
+//# sourceMappingURL=all.js.map
